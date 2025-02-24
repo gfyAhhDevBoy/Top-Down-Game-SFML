25044729e776de349aa14394784e40f06fd75c50
Tim Nikitin <tim.nikitin@posteo.de>
2025-02-24 13:49:28 +0100

    hrtsh

--- src/Entity.h
+++ src/Entity.h
@@ -5,16 +5,11 @@
 #include <iostream>
 #include <string>
 
-enum Entity_type {
-  ENTITY_INTERACTABLE,
-  ENTITY_STATIC,
-};
-
 class Entity : sf::Sprite {
  public:
   Entity() = default;
-  Entity(Entity_type entity_type, std::string &path, sf::Vector2i initial_pos)
-      : type(entity_type), position(initial_pos) {
+  Entity(std::string &path, sf::Vector2i initial_pos)
+      : position(initial_pos) {
     load_texture(path);
   }
 
@@ -26,9 +21,6 @@ class Entity : sf::Sprite {
 
  private:
   void load_texture(std::string &path);
-
-  Entity_type type;
-
   sf::Vector2i position;
   sf::Texture tex;
 };
