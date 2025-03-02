485ab1da6902d7062a2d3a50830a675cdf04a96f
gfyAhhDevBoy <tim.nikitin@posteo.de>
2025-03-02 19:13:44 +0100

    gerger

--- src/Game.cpp
+++ src/Game.cpp
@@ -5,11 +5,7 @@ void Game::init()
     window = new sf::RenderWindow(sf::VideoMode({800u, 600u}), "Game", sf::Style::Resize | sf::Style::Close);
     window->setFramerateLimit(60);
 
-<<<<<<< HEAD
-    // ToDo fix preloading
-=======
     // To Do fix file paths on windows
->>>>>>> 66522254f7aedd64837521be23da71a22a24d058
     ResourceManager::preload_textures("res");
     player = new Player(500, 500, ResourceManager::get_texture("res\\player.png"));
     test = new GameObject(200, 200, ResourceManager::get_texture("res/idk.png"));
