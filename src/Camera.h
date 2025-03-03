e61e6855ff5be17d11da3b93647615b5447485c3
gfyAhhDevBoy <tim.nikitin@posteo.de>
2025-03-03 13:19:55 +0100

    Camera started

--- src/Camera.h
+++ src/Camera.h
@@ -1,17 +1,27 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#include "GameObject.h"
 #include "Player.h"
+
+#include <vector>
 
 class Camera
 {
   public:
-    Camera(Player *plyr) : player(plyr)
+    Camera(Player *plyr, std::vector<GameObject *> &objs) : player(plyr), game_objs(objs)
     {
     }
 
+    void set_focus(GameObject *obj);
+    void move(sf::Vector2f vec);
+    void move(float x, float y);
+
   private:
     Player *player;
+    std::vector<GameObject *> game_objs;
+
+    GameObject *current_focus;
 };
 
 #endif
